97ed17bd1b4a821ef56d84e1b00ca093b638a3aa
Bart Boerman <bwboerman@gmail.com>
2018-01-08 14:30:39 +0100

    Delete outliers from train data

--- kaggle-housing-prices/outliers.R
+++ kaggle-housing-prices/outliers.R
@@ -6,18 +6,22 @@
 ## SalePrice > 650000
 ## LotArea   > 100000
 ## FirstFlrSf > 3000
-outliers.dt <- train.dt[GrLivArea > 4000 | SalePrice > 650000 | LotArea > 100000 | X1stFlrSF > 3000,] 
+outliers.Id <-  train.dt[GrLivArea > 4000 | SalePrice > 650000 | LotArea > 100000 | X1stFlrSF > 3000,Id]
+outliers.dt <- full.dt[Id %in% outliers.Id,]
 ###################################################################
-#### IQR (interquartile range)                                 ####
+#### IQR (interquartile range) sale price                      ####
 ###################################################################
 salePrice.iqr <- quantile(train.dt$SalePrice, 0.75)[[1]] + 1.5 * IQR(train.dt$SalePrice) ### 3th quantatile + (1.5 * IQR)
 outliers.SalePrice.dt <- train.dt[SalePrice > salePrice.iqr ,] 
 nrow(outliers.SalePrice.dt) ## 61 records maybe to many for just removing the records (4,1%)
 train.dt[SalePrice > salePrice.iqr ,.(count=.N), by=Neighborhood][order(Neighborhood)]
 train.dt[,.(count=.N), by=Neighborhood][order(Neighborhood)]
-#### Alternatives
+#### Alternatives (to do)
 ## Set value to 0.95 quantile
 ## Analyse outliers per neighborhood
 ###################################################################
 #### Delete outliers                                           ####
 ###################################################################
+full.dt <- full.dt[!(Id %in% outliers.Id),]
+
+
