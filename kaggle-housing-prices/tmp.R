0816bb9cfaddd2067d135300cc8479d4be7275b3
Bart  Boerman <31803510+BartBoerman@users.noreply.github.com>
2018-01-10 13:52:02 +0100

    Top 24% Kaggle

--- kaggle-housing-prices/tmp.R
+++ kaggle-housing-prices/tmp.R
@@ -26,6 +26,9 @@ cor(na.omit(full.dt[,c(variablesSquareFootage, variablesCounts), with = F]))
 summary(full.dt[,c(variablesSquareFootage), with = F])
 
 
+
+
+
 salePrice.stat  <- train.dt[,list(count=.N,
                                            mean=mean(SalePrice),
                                            min=min(SalePrice),
@@ -55,12 +58,7 @@ IQR(full.dt$SalePrice, na.rm = TRUE)
 
                             ]
 
-## The general form of data.table syntax is:
-##  DT[ i,  j,  by ] # + extra arguments
-##      |   |   |
-##      |   |    -------> grouped by what?
-##      |    -------> what to do?
-##       ---> on which rows?
+
 
 train.dt[,SalePriceLog:=log(SalePrice)]
 
@@ -95,6 +93,109 @@ train.dt[,.(count=.N,
                     iqr=IQR(SalePrice, na.rm = T)) 
                     #,by='Neighborhood'
          ]
+
+
+
+## Analyse mean imputation LotFrontage
+tmp.dt <- copy(full.dt)
+tmp.dt <- full.dt[, c("Id","Neighborhood","LotFrontage")]
+tmp.dt[,.(count=.N,
+            mean=mean(LotFrontage, na.rm = T),
+            median=median(LotFrontage, na.rm = T),
+            quantile_upper=quantile(LotFrontage, .75, na.rm=T),
+            max=max(LotFrontage, na.rm = T)) 
+         ,by=Neighborhood
+         ][order(Neighborhood)]
+
+
+tmp.dt[, LotFrontageMean := replace(LotFrontage, is.na(LotFrontage), median(LotFrontage, na.rm=T)), by=.(Neighborhood)]
+
+### Analyse imputation MSZoning
+tmp.dt <- copy(full.dt)
+tmp.dt <- full.dt[, c("Id","Neighborhood","MSZoning","SalePrice","LotArea","GrLivArea")]
+tmp.dt <- full.dt[is.na(MSZoning),]
+Neighborhood.MSZoning.missing <- unique(full.dt[is.na(MSZoning), c("Neighborhood")])
+Neighborhood.MSZoning <- unique(full.dt[!is.na(MSZoning), c("Neighborhood","MSZoning")][order(Neighborhood)])
+
+  
+train.dt[Neighborhood %in% Neighborhood.MSZoning.missing$Neighborhood,.(count=.N,
+            mean=mean(SalePrice),
+            min=min(SalePrice),
+            max=max(SalePrice)) 
+         ,by=c("Neighborhood", "MSZoning") 
+         ][order (Neighborhood,MSZoning)]
+
+train.dt[Neighborhood %in% Neighborhood.MSZoning.missing$Neighborhood,.(count=.N,
+                                                                        mean=mean(GrLivArea),
+                                                                        min=min(GrLivArea),
+                                                                        max=max(GrLivArea)) 
+         ,by=c("Neighborhood", "MSZoning") 
+         ][order (Neighborhood,MSZoning)]
+
+## RL for missing MSZoning in Mitchel because GrLivArea is greater then max of RM
+## Not sure (yet) for missing MSZoning in IDOTRR. RM is most common in IDOTRR but might be wrong
+
+## Electrical
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","MSZoning","Electrical","SalePrice","LotArea")]
+
+full.dt[Neighborhood == "Timber",.(count=.N) 
+       ,by = c("Neighborhood", "MSZoning","Electrical") 
+       ][order (Neighborhood, MSZoning, Electrical)]
+
+tmp.dt <- tmp.dt[is.na(Electrical), ]
+## Most common value for neihborhood Timber is SBrkr
+
+## Utilities
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","MSZoning","Electrical","Utilities","SalePrice","LotArea")]
+
+full.dt[Neighborhood == "Timber",.(count=.N) 
+        ,by = c("Neighborhood", "MSZoning","Electrical") 
+        ][order (Neighborhood, MSZoning, Electrical)]
+
+tmp.dt <- tmp.dt[is.na(Utilities) | Utilities == "NoSeWa", ]
+
+## porche
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","OpenPorchSF", "EnclosedPorch", "ThreeSsnPorch", "ScreenPorch")]
+tmp.dt[,porchSf := (OpenPorchSF + EnclosedPorch + ThreeSsnPorch + ScreenPorch)]
+
+## Exterior2nd Exterior1st
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","MSZoning","OverallQual","OverallCond","Exterior2nd","Exterior1st","SalePrice","LotArea","totalSF")]
+tmp.dt <- tmp.dt[Neighborhood == "Edwards" & OverallQual == 5 & OverallCond ==7,]
+
+tmp.dt[,.(count=.N, meanSalePrice=mean(SalePrice)) 
+        ,by = c("Exterior1st", "Exterior2nd") 
+        ][order (Exterior1st, Exterior2nd)]
+
+### MasVnrType and MasVnrArea
+
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","MSZoning","OverallQual","OverallCond","Functional","Exterior2nd","Exterior1st","MasVnrType","MasVnrArea","SalePrice","LotArea","totalSF")]
+tmp.dt <- tmp.dt[Exterior1st %in% c("VinylSd","Wd Shng","CmentBd"),]
+
+tmp.dt[,.(count=.N, meanSalePrice=mean(SalePrice, na.rm = T)) 
+       ,by = c("Neighborhood", "Exterior1st","MasVnrType") 
+       ][order (Neighborhood, Exterior1st, MasVnrType)]
+
+## Functional
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","Neighborhood","MSZoning","OverallQual","OverallCond","Functional","Utilities","Exterior2nd","Exterior1st","MasVnrType","MasVnrArea","SalePrice","LotArea","totalSF")]
+
+## Time
+
+tmp.dt <- copy(full.dt)
+tmp.dt <- tmp.dt[, c("Id","MoSold","YrSold","Neighborhood","MSZoning","OverallQual","OverallCond","Functional","Exterior2nd","Exterior1st","MasVnrType","MasVnrArea","SalePrice","LotArea","totalSF")]
+
+
+
+
+
+
+
+
 
 
 
