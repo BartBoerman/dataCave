74e2564ac29ed897ab03b342e39416a47cda0428
Bart Boerman <bwboerman@gmail.com>
2018-01-12 12:31:04 +0100

    remove factor columns after creating dummies

--- kaggle-housing-prices/dummy-variables.R
+++ kaggle-housing-prices/dummy-variables.R
@@ -7,3 +7,5 @@ f <- paste('~', paste(variablesFactor, collapse = ' + '))
 encoder <- dummyVars(as.formula(f), full.dt, fullRank = T, drop2nd = T)
 full.dummyVars.dt <- as.data.table(predict(encoder, full.dt))
 full.dt <- cbind(full.dt, full.dummyVars.dt)
+## remove factor columns
+full.dt <- full.dt[, (variablesFactor):=NULL]
