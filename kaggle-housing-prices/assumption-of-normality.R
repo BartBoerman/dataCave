bdaa4b6128e26b3ff88ee198e3ec562447af9718
Bart Boerman <bwboerman@gmail.com>
2018-01-12 16:18:18 +0100

    plots and transforms to solve skewness

--- kaggle-housing-prices/assumption-of-normality.R
+++ kaggle-housing-prices/assumption-of-normality.R
@@ -1,11 +1,49 @@
 ## https://stats.stackexchange.com/questions/52293/r-qqplot-how-to-see-whether-data-are-normally-distributed
 ###############################################################################
-#### assumption of normality                                               ####
+#### Skewness and kurtosis                                                 ####                                                   ####
 ###############################################################################
+## http://rcompanion.org/handbook/I_12.html
+
+require(e1071)
+
+full.dt <- full.dt[!(Id %in% outliers.Id),]
+
+x <- na.omit(full.dt$SalePrice)
+
+# skewness and kurtosis, they should be around (0,3)
+skewness(x)
+kurtosis(x)
+
+# histogramm
+hist(x)
+
+# qq-plot: you should observe a good fit of the straight line
+qqnorm(x,  ylab="Sample Quantiles for x")
+qqline(x,  col="red")
+
+# p-plot: you should observe a good fit of the straight line
+probplot(x, qdist=qnorm)
+
+require(car)
+
+  qqPlot(x, distribution="norm")
+
+
+require(rcompanion)
+  
+plotNormalHistogram(x)
+plotNormalHistogram(sqrt(x))
+plotNormalHistogram(log(x))
+T_cub = sign(x) * abs(x)^(1/3)  ## Cube root transformation
+plotNormalHistogram(T_cub)
+
+T_tuk = transformTukey(x, plotit=FALSE) ## Tukey’s Ladder of Powers transformation
+plotNormalHistogram(T_tuk)
+
+
 ###############################################################################
-#### Box cox transformation on response variable                           ####                                                   ####
+#### Inverse of BoxCoxTrans does not exist in Caret                        ####                                                   ####
 ###############################################################################
-## Inverse of BoxCoxTrans does not exist in Caret
 BoxCoxTrans.inverse <- function(object, newdata) {
   if(!is.vector(newdata) || !is.numeric(newdata)) stop("newdata should be a numeric vector")
   if(is.na(object$lambda)) return(newdata) 
@@ -17,17 +55,20 @@ BoxCoxTrans.inverse <- function(object, newdata) {
   
   if(lambda == 0) exp(newdata) else (lambda*newdata + 1)^(1/lambda) 
 }
-## Try outs
-b <- BoxCoxTrans(train.dt$SalePrice)
+###############################################################################
+#### BoxCox transform response variable                                    ####                                                   ####
+###############################################################################
+require(caret)
+T_box.fit <- BoxCoxTrans(y = full.dt$SalePrice, x = full.dt$GrLivArea, na.rm = TRUE)
+T_box <- predict(b2, train.dt$SalePrice)
+T_box.inverse <- BoxCoxTrans.inverse(b2,p)
+T_box.compare <- cbind(train.dt$SalePrice,r)
 
-p <- predict(b, train.dt$SalePrice)
+plotNormalHistogram(T_box)
+###############################################################################
+#### BoxCox transform response variable                                    ####                                                   ####
+###############################################################################
+T_box.fit <- BoxCoxTrans(y = full.dt$SalePrice, na.rm = TRUE)
+T_box <- predict(b2, train.dt$SalePrice)
+plotNormalHistogram(T_box)
 
-r <- BoxCoxTrans.inverse(b,p)
-
-z <- cbind(train.dt$SalePrice,r)
-
-
-b2 <- BoxCoxTrans(y = full.dt$SalePrice, x = full.dt$GrLivArea, na.rm = TRUE)
-p <- predict(b2, train.dt$SalePrice)
-r <- BoxCoxTrans.inverse(b2,p)
-z <- cbind(train.dt$SalePrice,r)
