05db6d7d23fee3aa6cf4d0df86c9489670cdb989
Bart Boerman <bwboerman@gmail.com>
2018-01-16 12:45:54 +0100

    Move setting order for ordinal factors to separate file

--- kaggle-housing-prices/data-table.R
+++ kaggle-housing-prices/data-table.R
@@ -121,38 +121,6 @@ full.dt[,(changeColType):= lapply(.SD, as.numeric), .SDcols = changeColType]
 changeColType <- variablesFactor
 full.dt[,(changeColType):= lapply(.SD, as.factor), .SDcols = changeColType]
 ###################################################################
-#### Ordered factors                                           ####
-###################################################################
-## OverallQual, rates the overall material and finish of the house
-full.dt[,OverallQual:=ordered(OverallQual, levels = c(1:10))]
-## OverallCond, rates the overall condition of the house
-full.dt[,OverallCond:=ordered(OverallCond, levels = c(1:10))]
-## KitchenQual, kitchen quality
-full.dt[,KitchenQual:=ordered(KitchenQual, levels = c("Po","Fa","TA","Gd","Ex"))]
-## GarageFinish (contains NA's)
-full.dt[,GarageFinish:=ordered(GarageFinish, levels = c("None","Unf","RFn","Fin"))]
-## ExterQual, evaluates the quality of the material on the exterior  
-full.dt[,ExterQual:=ordered(ExterQual, levels = c("Po","Fa","TA","Gd","Ex"))]
-## ExterCond, evaluates the present condition of the material on the exterior
-full.dt[,ExterCond:=ordered(ExterCond, levels = c("Po","Fa","TA","Gd","Ex"))]
-## BsmtQual (contains NA's), evaluates the height of the basement
-full.dt[,BsmtQual:=ordered(BsmtQual, levels = c("None","Po","Fa","TA","Gd","Ex"))]
-## BsmtCond (contains NA's), evaluates the general condition of the basement
-full.dt[,BsmtCond:=ordered(BsmtCond, levels = c("None","Po","Fa","TA","Gd","Ex"))]
-## BsmtExposure (contains NA's), refers to walkout or garden level walls
-full.dt[,BsmtExposure:=ordered(BsmtExposure, levels = c("None","No","Mn","Av","Gd"))]
-## BsmtFinType1 (contains NA's), rating of basement finished area
-full.dt[,BsmtFinType1:=ordered(BsmtFinType1, levels = c("None","Unf","LwQ","Rec","BLQ","ALQ","GLQ"))]
-## FireplaceQu (contains NA's), fireplace quality
-full.dt[,FireplaceQu:=ordered(FireplaceQu, levels = c("None","Po","Fa","TA","Gd","Ex"))]
-## Electrical
-full.dt[,Electrical:=ordered(Electrical, levels = c("FuseP","Mix","FuseF","FuseA","SBrkr"))]
-## Fence
-full.dt[,Fence:=ordered(Fence, levels = c("None","MnWw","MnPrv","GdWo","GdPrv"))]
-## PoolQC
-full.dt[,FireplaceQu:=ordered(FireplaceQu, levels = c("None","Fa","Gd","Ex"))]
-## Ordered factors are not supported by h2o, Let's convert them into integers during pre-processing. Lowest level will be 1 etc.
-###################################################################
 #### Descriptive statistics                                    ####
 ###################################################################
 ## statisticts
@@ -229,29 +197,31 @@ full.dt[is.na(Fence), Fence := "None"]
 ###################################################################
 #### Feature engineering                                       ####
 ###################################################################
-full.dt[, ':=' (totalSF = TotalBsmtSF + FirstFlrSF + SecondFlrSF,
-               hasRegLotShape = ifelse(LotShape == "Reg",1,0),
-               hasLvlLandContour = ifelse(LandContour == "Lvl",1,0),
-               hasGtlLandSlope = ifelse(LandSlope == "Gtl",1,0),
-               hasElectricalSBrkr = ifelse(Electrical == "SBrkr",1,0),
-               hasPavedDrive = ifelse(PavedDrive == "Y",1,0),
-               hasShed = ifelse(MiscFeature == "Shed",1,0),
-               hasOpenPorch = ifelse(OpenPorchSF > 0,1,0),
-               hasEnclosedPorch = ifelse(EnclosedPorch > 0,1,0),
-               hasThreeSsnPorch = ifelse(ThreeSsnPorch > 0,1,0),
-               hasScreenPorch = ifelse(ScreenPorch > 0,1,0),
-               hasPorch = ifelse(ScreenPorch > 0 | ThreeSsnPorch > 0 | EnclosedPorch > 0 | OpenPorchSF > 0, 1, 0),
-               hasSecondFloor = ifelse(SecondFlrSF > 0,1,0),
-               hasHasMasVnr = ifelse(MasVnrArea > 0,1,0),
-               hasHasWoodDeck = ifelse(WoodDeckSF > 0,1,0),
-               hasBadHeating = ifelse(HeatingQC %in% c("Fa","Po"),1,0),
-               isNewerDwelling = ifelse(MSSubClass %in% c(20,60,120,160),1,0),
-               isRemodeled = ifelse(YearRemodAdd == YearBuilt, 1, 0),
-               isRemodeledRecent = ifelse(YearRemodAdd == YrSold, 1, 0),
-               isNew = ifelse(YearBuilt == YrSold, 1, 0),
-               ageInYears = 2010 - YearBuilt,
-               yearsSinceSold = 2010 - YrSold
-)]
+full.dt[, totalSF:= (TotalBsmtSF + FirstFlrSF + SecondFlrSF)]
+
+# full.dt[, ':=' (totalSF = TotalBsmtSF + FirstFlrSF + SecondFlrSF,
+#                hasRegLotShape = ifelse(LotShape == "Reg",1,0),
+#                hasLvlLandContour = ifelse(LandContour == "Lvl",1,0),
+#                hasGtlLandSlope = ifelse(LandSlope == "Gtl",1,0),
+#                hasElectricalSBrkr = ifelse(Electrical == "SBrkr",1,0),
+#                hasPavedDrive = ifelse(PavedDrive == "Y",1,0),
+#                hasShed = ifelse(MiscFeature == "Shed",1,0),
+#                hasOpenPorch = ifelse(OpenPorchSF > 0,1,0),
+#                hasEnclosedPorch = ifelse(EnclosedPorch > 0,1,0),
+#                hasThreeSsnPorch = ifelse(ThreeSsnPorch > 0,1,0),
+#                hasScreenPorch = ifelse(ScreenPorch > 0,1,0),
+#                hasPorch = ifelse(ScreenPorch > 0 | ThreeSsnPorch > 0 | EnclosedPorch > 0 | OpenPorchSF > 0, 1, 0),
+#                hasSecondFloor = ifelse(SecondFlrSF > 0,1,0),
+#                hasHasMasVnr = ifelse(MasVnrArea > 0,1,0),
+#                hasHasWoodDeck = ifelse(WoodDeckSF > 0,1,0),
+#                hasBadHeating = ifelse(HeatingQC %in% c("Fa","Po"),1,0),
+#                isNewerDwelling = ifelse(MSSubClass %in% c(20,60,120,160),1,0),
+#                isRemodeled = ifelse(YearRemodAdd == YearBuilt, 1, 0),
+#                isRemodeledRecent = ifelse(YearRemodAdd == YrSold, 1, 0),
+#                isNew = ifelse(YearBuilt == YrSold, 1, 0),
+#                ageInYears = 2010 - YearBuilt,
+#                yearsSinceSold = 2010 - YrSold
+# )]
 
 
 
