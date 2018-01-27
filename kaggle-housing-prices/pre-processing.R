075476b991900a4b3fa9224bd7c996355e49ad6b
Bart Boerman <bwboerman@gmail.com>
2018-01-27 16:45:56 +0100

    normalize with log1p scale instead of rebust scale split based on year sold reverted back to just removing high outliers

--- kaggle-housing-prices/pre-processing.R
+++ kaggle-housing-prices/pre-processing.R
@@ -2,21 +2,20 @@
 #### Dependencies                                              ####
 ###################################################################
 require(caret)      # (near) zero variance and dummyVars
-require(robustHD)   # robust standardize 
 ###################################################################
 #### Pre-processing                                            ####
 ###################################################################
 ##### remove outliers
 outliersHigh.Id <-  train.dt[GrLivArea > 4000 | LotArea > 100000 | X1stFlrSF > 3000 | GarageArea > 1200,Id]
-outliersLowPrice.Id <-  train.dt[(GarageArea > 1000 & SalePrice  < 200000) |
-                            (GrLivArea > 4000 & SalePrice   < 200000) | 
-                            (TotalBsmtSF > 6000 & SalePrice < 200000) |
-                            (TotRmsAbvGrd > 12 & SalePrice  < 300000), Id]
-full.dt <- full.dt[!(Id %in% outliersHigh.Id) & !(Id %in% outliersLowPrice.Id)]
+#outliersLowPrice.Id <-  train.dt[(GarageArea > 1000 & SalePrice  < 200000) |
+#                            (GrLivArea > 4000 & SalePrice   < 200000) | 
+#                            (TotalBsmtSF > 6000 & SalePrice < 200000) |
+#                            (TotRmsAbvGrd > 12 & SalePrice  < 300000), Id]
+full.dt <- full.dt[!(Id %in% outliersHigh.Id)]
 #### create index for splitting data 
 setkey(full.dt,dataPartition)
 train.full.dt <- full.dt["train"]
-index <- createDataPartition(train.full.dt$OverallQual, p=0.80, list=FALSE)
+index <- createDataPartition(train.full.dt$YrSold, p=0.80, list=FALSE)
 #### remove unwanted variables
 full.dt[, (variablesDrop):=NULL]
 full.dt[, (variablesFactor):=NULL]
@@ -29,14 +28,14 @@ full.dt[,(changeColType):= lapply(.SD, as.integer), .SDcols = changeColType]
 ## log transform skewed variables (including response variable)
 skewedVariables <- sapply(full.dt[, c(variablesSquareFootage,variablesValues), with = FALSE],function(x){skew(x,na.rm=TRUE)})
 ## keep only features that exceed a threshold for skewness
-skewedVariables <- skewedVariables[skewedVariables > 0.75]
+skewedVariables <- skewedVariables[skewedVariables > 0.50]
 ## transform excessively skewed features with log
 skewedVariables <- names(skewedVariables)
-full.dt[, (skewedVariables) := lapply(.SD, function(x) log(x)), .SDcols = skewedVariables]
+full.dt[, (skewedVariables) := lapply(.SD, function(x) log1p(x)), .SDcols = skewedVariables]
 #### scale (Done in h20 algorithm )
 ## scale (exluding response)
 varScale <- setdiff(c(variablesSquareFootage, variablesValues), c(response)) ## Do not scale response
-full.dt[, (varScale) := lapply(.SD, function(x) robStandardize(x, centerFun = median, scaleFun = mad)), .SDcols = varScale]
+full.dt[, (varScale) := lapply(.SD, function(x) scale(x, center = T, scale = T)), .SDcols = varScale]
 ###################################################################
 #### Select features                                           ####
 ###################################################################
@@ -50,3 +49,4 @@ test.dt <- full.dt["test"]
 #Spliting training set into train and validate
 train.dt <- train.full.dt[index,]
 validate.dt <- train.full.dt[-index,]
+
