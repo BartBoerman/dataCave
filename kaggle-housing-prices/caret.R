30da725357f819a3b10afe5c16fe4117599f2572
Bart Boerman <bwboerman@gmail.com>
2018-01-29 11:49:52 +0100

    Initial version

---
+++ kaggle-housing-prices/caret.R
@@ -0,0 +1,37 @@
+require(caret)
+require(caretEnsemble)
+require(foreach)
+require(iterators)
+require(parallel)
+require(doParallel)
+cluster <- makeCluster(detectCores()-1)  # leave one for OS
+registerDoParallel(cluster)
+## https://cran.r-project.org/web/packages/caretEnsemble/vignettes/caretEnsemble-intro.html
+my_control <- trainControl(
+  method="boot",
+  number=25,
+  savePredictions="final",
+  index=createResample(train.dt$OverallQual, 20),
+  allowParallel =TRUE
+)
+
+formula.all <- as.formula(paste("SalePrice ~ ", paste(features, collapse= "+"))) 
+
+model_list <- caretList(
+  formula.all, data=train.dt,
+  trControl=my_control,
+  methodList=c("svmRadial", "glm","xgbTree")
+)
+################################################### 
+####  return to single threaded processing     #### 
+###################################################
+stopCluster(cluster)
+registerDoSEQ()
+
+
+p <- as.data.frame(predict(model_list, newdata=head(validate.dt)))
+print(p)
+
+
+
+
